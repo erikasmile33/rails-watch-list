27dddf2f86b98a0105265f51e37163992c819f6b
Xinyi Wang <rena.wxy@gmail.com>
2024-07-06 15:50:30 +0900

    list index and show working

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new
+    if @list.save
+      redirect_to
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+end
+
+private
+
+def list_params
+  params.require(:movie).permit(:title, :overview)
+end
