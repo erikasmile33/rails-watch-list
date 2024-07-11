208de4fe12720e2f8a46bfa440be502b684e190c
Xinyi Wang <rena.wxy@gmail.com>
2024-07-11 20:16:53 +0900

    Made the bookmark part work

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,33 @@
 class BookmarksController < ApplicationController
+  before_action :set_list
+
+  def new
+    @bookmark = @list.bookmarks.new
+  end
+
+  def create
+    @bookmark = @list.bookmarks.new(bookmark_params)
+    debugger
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = @list.Bookmarks.find(params[:id])
+    @bookmark.destroy
+    list_path(@bookmark.list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
 end
